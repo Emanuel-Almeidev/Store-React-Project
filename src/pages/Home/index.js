1440fe7b116129f811fd756fcad6ac2153b3cb0b
Emanuel <emvictorel@outlook.com>
2020-06-14 19:03:17 -0300

    estilizando a Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
 
+import { MdAddShoppingCart } from 'react-icons/md'
+
+import { ProductList } from './styles'
+
 export default function Home(){
     return (
-        <h1>Home</h1>
+        <ProductList>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
+                alt="tênis"/>
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff" size={16}/> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
+                alt="tênis"/>
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff" size={16}/> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
+                alt="tênis"/>
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff" size={16}/> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
+                alt="tênis"/>
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff" size={16}/> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+        </ProductList>
     )
 }
