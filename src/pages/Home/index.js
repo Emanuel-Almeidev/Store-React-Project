5aa0c58c395c817f265bcec5b80c64e428ef1bee
Emanuel <emvictorel@outlook.com>
2020-06-14 21:22:57 -0300

    Consumindo dados de produtos a partir de uma fake api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,61 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md'
 
+import { formatPrice }  from '../../util/format'
+
 import { ProductList } from './styles'
 
-export default function Home(){
-    return (
-        <ProductList>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
-                alt="tênis"/>
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16}/> 3
-                    </div>
-                    <span>Adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
-                alt="tênis"/>
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16}/> 3
-                    </div>
-                    <span>Adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
-                alt="tênis"/>
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16}/> 3
-                    </div>
-                    <span>Adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" 
-                alt="tênis"/>
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16}/> 3
-                    </div>
-                    <span>Adicionar ao carrinho</span>
-                </button>
-            </li>
+import api from '../../services/api'
 
-        </ProductList>
-    )
+class Home extends Component{
+
+    state = {
+        products: [],
+    }
+
+    async componentDidMount(){
+        const response = await api.get('/products')
+
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price) 
+        }))
+        
+        this.setState({
+            products: data,
+        })
+    }
+
+    
+
+    render(){
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} 
+                        alt="tênis"/>
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart color="#fff" size={16}/> 3
+                            </div>
+                            <span>Adicionar ao carrinho</span>
+                        </button>
+                    </li>
+                ))}
+
+    
+            </ProductList>
+        )
+    }
 }
+
+export default Home;
